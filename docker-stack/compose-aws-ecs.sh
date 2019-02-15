b951b26d45c6b1ebbd8d057e63376af6537d3f3d
root <root@xms1.secom.net>
2019-02-15 08:31:38 -0700

    Use ECS service mode instead of task mode

--- docker-stack/compose-aws-ecs.sh
+++ docker-stack/compose-aws-ecs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-export $(grep -v '^#' ./aws-ecs.env | xargs -d '\n') && ecs-cli compose -f docker-compose.base.yml -f docker-compose.aws-ecs.yml -p secom-phpipam $*
+export $(grep -v '^#' ./aws-ecs.env | xargs -d '\n') && ecs-cli compose -f docker-compose.base.yml -f docker-compose.aws-ecs.yml -p secom-phpipam service $*
